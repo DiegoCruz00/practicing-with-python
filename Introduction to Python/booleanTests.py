8b3e06a03a6f9269a84f33999a417c6a4d28edc4
Diego Cruz <diego.cruz001@gmail.com>
2020-04-07 08:28:03 -0300

    Boolean studies and examples created

---
+++ Introduction to Python/booleanTests.py
@@ -0,0 +1,20 @@
+num1 = 0
+num2 = 1
+num3 = 87
+
+var = None
+
+emptyString = ""
+string = "text"
+
+emptyArray = []
+array = [0, 2, 32]
+
+print(" > bool(num1): {}".format(bool(num1)))
+print(" > bool(num2): {}".format(bool(num2)))
+print(" > bool(num3): {}".format(bool(num3)))
+print(" > bool(var): {}".format(bool(var)))
+print(" > bool(emptyString): {}".format(bool(emptyString)))
+print(" > bool(string): {}".format(bool(string)))
+print(" > bool(emptyArray): {}".format(bool(emptyArray)))
+print(" > bool(array): {}".format(bool(array)))
